95f8569436e1e3becaa481ad3dd525808638d030
rikcsilva <66225602+rikcsilva@users.noreply.github.com>
2021-04-07 17:20:39 -0300

    Update and rename Lista I exercício 6.py to Tempo_viagem.py

--- Tempo_viagem.py
+++ Tempo_viagem.py
@@ -1,3 +1,5 @@
+#Calcule o tempo de uma viagem de carro. Pergunte a distância a percorrer e a velocidade média esperada para a viagem
+
 d = float(input('Distância em km: '))
 v = float(input('Velocidade média em km/h: '))
 t = d / v
