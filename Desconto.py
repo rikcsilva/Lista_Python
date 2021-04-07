8654bcf61499980d1ec6d855c87e2b7f7c61bb50
rikcsilva <66225602+rikcsilva@users.noreply.github.com>
2021-04-07 17:19:54 -0300

    Update and rename Lista I exercício 5.py to Desconto.py

--- Desconto.py
+++ Desconto.py
@@ -1,3 +1,5 @@
+#Solicite o preço de uma mercadoria e o percentual de desconto. Exiba o valor do desconto e o preço a pagar
+
 m = float(input('Preço da mercadoria: '))
 p = float(input('Porcentagem de desconto: '))
 desconto = m * p / 100
