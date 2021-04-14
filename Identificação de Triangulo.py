7c47e093c8feba391949115078fdbd0aef2ed8ec
rikcsilva <66225602+rikcsilva@users.noreply.github.com>
2021-04-14 14:22:24 -0300

    Update and rename Lista II exercício 1.py to Identificação de Triangulo.py
    Faça um Programa que peça os três lados de um triângulo. O programa deverá informar se os valores podem ser um triângulo. Indique, caso os lados formem um triângulo, se o mesmo é: equilátero, isósceles ou escaleno.

--- Identificação de Triangulo.py
+++ Identificação de Triangulo.py
@@ -1,3 +1,7 @@
+# Faça um Programa que peça os três lados de um triângulo. 
+# O programa deverá informar se os valores podem ser um triângulo. 
+# Indique, caso os lados formem um triângulo, se o mesmo é: equilátero, isósceles ou escaleno.
+
 a = int(input('Lado a: '))
 b = int(input('Lado b: '))
 c = int(input('Lado c: '))
